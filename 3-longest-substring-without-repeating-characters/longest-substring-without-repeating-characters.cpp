f39a0f43772e3b51b9bf2f08a91b0a66aa875d09
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-02-06 19:47:01 +0530

    Time: 2679 ms (5.03%) | Memory: 482 MB (5.01%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+	   if(s.empty()) return 0;
+	
+    if(s==" ") return 1;
+
+	
+	    map<string,int> lenCount;
+	    int count=0; string ans;
+	    map<int,int> cntAlpha;
+	    
+	    for(int i=0; i<s.length(); i++){
+	    	// cout<<i<<endl;
+            // if(s[i]==' ') continue;
+		    cntAlpha[s[i]-'a']++;
+		    if(cntAlpha[s[i]-'a']==1){
+			    count++;
+			    ans+=s[i];
+			    lenCount.insert({ans,count});
+		    }
+		    else{
+		    	i=i-count;
+			    // cntAlpha[s[i]-'a']=0;
+			    // fill(cntAlpha.begin(), cntAlpha.end(), 0);
+                cntAlpha.clear();
+			    count=0;
+			    ans="";
+		    }
+	    }
+	    int len=INT_MIN;
+	    for(auto i: lenCount){
+		    // cout<<i.first<<" "<<i.second<<endl;
+		    len=max(len,i.second);
+	    }
+	    //map<string, int>::iterator itr;
+
+        // Display the last element in m.
+  	    // itr = lenCount.end();
+  	    // --itr;
+  	
+  	    // int len=itr->second;
+          if(len==INT_MIN) return 1;
+
+  	    return len;
+    }
+};
