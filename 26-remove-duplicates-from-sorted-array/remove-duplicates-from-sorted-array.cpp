3ff20fa78225cccca16c6bcf63c263449516c82c
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-02-11 12:54:38 +0530

    Time: 8 ms (61.81%) | Memory: 22.3 MB (5.74%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> ans;
+        // vector<int> ans;
+        // for(int i=0; i<nums.size(); i++){
+        //     if( i+1<nums.size()  && nums[i]!=nums[i+1])
+        //         ans.push_back(nums[i]);
+        // }
+        // ans.push_back(nums[nums.size()-1]);
+        set<int> ans;
         for(int i=0; i<nums.size(); i++){
-            if( i+1<nums.size()  && nums[i]!=nums[i+1])
-                ans.push_back(nums[i]);
+            ans.insert(nums[i]);
         }
-        ans.push_back(nums[nums.size()-1]);
-        int k=ans.size();
+        
         //assert k == expectedNums.length;
+        vector<int> temp;
+        set<int>::iterator it;
+         for (it = ans.begin(); it != ans.end(); it++){
+    	    //cout << *it << " ";
+            temp.push_back(*it);
+        }
+        int k=temp.size();
         for (int i = 0; i < k; i++) {
-            nums[i] = ans[i];
+            nums[i] = temp[i];
         }
 
         return k;
