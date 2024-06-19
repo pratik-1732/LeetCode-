97595e761c11867fb7c097f2bcf6a2f531bd1a6a
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-06-19 08:55:26 +0530

    Time: 4 ms (56.07%) | Memory: 14.4 MB (86.85%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,43 +1,41 @@
+#include <bits/stdc++.h>
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-    int index,indexNum,cnt=0;
-	for(int i=nums.size()-1; i>0; i--){
-		if(nums[i]>nums[i-1]){
-			index=i-1;
-			indexNum=nums[i-1];
-			break;
-		}
-		else cnt++;
-	}
-    // cout<<"cnt-"<<cnt<<endl;
-	if(cnt==nums.size()-1){
-		sort(nums.begin(), nums.end());
-		for(auto i: nums){
-			cout<<i<<" ";
-		}
-	}
-	else{
-		int min = INT_MAX, ans;
-	
-		for(int i=index+1; i<nums.size(); i++){
-			if(nums[i]>indexNum){
-				int temp = nums[i]-indexNum;
-				if(temp<min){
-					min=temp;
-					ans=i;
-				}
-			// cout<<"tempi-"<<temp<<endl;
-			}
-		}
-		int temp= nums[ans];
-		nums[ans]=indexNum;
-		nums[index]=temp;
-        sort(nums.begin()+index+1, nums.end());
-		// cout<<temp<<endl;
-		for(auto i: nums){
-			cout<<i<<" ";
-		}
-	}
+        int diff=INT_MAX, minInd=0, n=nums.size();
+        // for(int i=1; i<n; i++){
+        //     if(nums[i]>nums[0]){
+        //         if(nums[i]-nums[0]<diff){
+        //             diff= nums[i]-nums[0];
+        //             minInd= i;
+        //         }
+        //     }
+        // }
+        int changeInd=-1;
+        for(int i=n-1; i>0; i--){
+            if(nums[i]>nums[i-1]){
+                changeInd=i-1;
+                break;
+            }
+        }
+        //cout<<changeInd<<endl;
+        if(changeInd==-1){
+            sort(nums.begin(), nums.end());
+            return;
+        }
+        diff=INT_MAX;
+        int nextInd;
+        for(int i=changeInd+1; i<n; i++){
+            if(nums[i]>nums[changeInd]){
+                if(nums[i]-nums[changeInd]<diff){
+                    diff= nums[i]-nums[changeInd];
+                    nextInd= i;
+                }
+            }
+        }
+        //cout<<nextInd<<endl;
+        swap(nums[changeInd], nums[nextInd]);
+        sort(nums.begin()+changeInd+1, nums.end());
+        
     }
 };
