84fa0530213d21c436c36650cfcd250986ce935d
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-07-08 10:05:19 +0530

    Time: 3 ms (33.65%) | Memory: 7.5 MB (69.20%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -13,6 +13,19 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return output(m-1, n-1, dp);
+        // return output(m-1, n-1, dp);
+        for(int row=0; row<m; row++){
+            for(int col=0; col<n; col++){
+                if(row==0 && col==0) dp[row][col]= 1;
+                else{
+                    int up=0, left=0;
+                    if(row>0) up= dp[row-1][col];
+                    if(col>0) left= dp[row][col-1];
+
+                    dp[row][col]= up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
