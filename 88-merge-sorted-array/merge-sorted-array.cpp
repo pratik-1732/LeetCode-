36585eee940202ac825f3b419de3a7c0ab5771b2
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-08-11 12:58:46 +0530

    Time: 6 ms (7.48%) | Memory: 11.3 MB (12.72%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,11 +1,36 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int j=0; 
-        for(int i=m; i<m+n; i++){
-            nums1[i]=nums2[j];
-            j++;
+        if(m==0){
+            nums1=nums2;
+            return;
+        } 
+        if(n==0){
+            return;
         }
-        sort(nums1.begin(), nums1.end());
+        int i=m-1, j=m+n-1, k; 
+        for(k=n-1; k>=0; k--){
+            if(i<0) break;
+            if(nums2[k]>nums1[i]){
+                nums1[j]=nums2[k];
+                j--;
+            }
+            else if(nums2[k]==nums1[i]){
+                nums1[j]=nums2[k];
+                j--; 
+                nums1[j]=nums1[i];
+                j--; i--;
+            }
+            else{
+                nums1[j]=nums1[i];
+                j--; i--;
+                k++;
+            }
+        }
+        cout<<j<<endl;
+            while(k>=0){
+                nums1[j]=nums2[k];
+                j--; k--;
+            }
     }
 };
