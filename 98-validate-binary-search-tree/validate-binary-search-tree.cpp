359be580794eeb270b2bce99f8f9c3332ec590bd
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-08-21 23:09:54 +0530

    Time: 8 ms (56.13%) | Memory: 20.5 MB (16.41%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -10,17 +10,34 @@
  * };
  */
 
+// class Solution {
+// public:
+//     // bool output(TreeNode* root, long long mini, long long maxi){
+//     //     if(root==NULL) return true;
+
+//     //     if(root->val<=mini || root->val>=maxi) return false;
+
+//     //     return output(root->left, mini, (long long)root->val) && output(root->right, (long long)root->val, maxi);
+//     // }
+//     // bool isValidBST(TreeNode* root) {
+//     //     long long mini=LLONG_MIN, maxi= LLONG_MAX;
+//     //     return output(root, mini, maxi);
+//     // }
+// };
+
 class Solution {
 public:
-    bool output(TreeNode* root, long long mini, long long maxi){
-        if(root==NULL) return true;
-
-        if(root->val<=mini || root->val>=maxi) return false;
-
-        return output(root->left, mini, (long long)root->val) && output(root->right, (long long)root->val, maxi);
+    vector<int> nodes;
+    void inorder(TreeNode* root) {
+        if (root->left) inorder(root->left);
+        nodes.push_back(root->val);
+        if (root->right) inorder(root->right);
     }
     bool isValidBST(TreeNode* root) {
-        long long mini=LLONG_MIN, maxi= LLONG_MAX;
-        return output(root, mini, maxi);
+        inorder(root);
+        for (int i = 0; i < nodes.size() - 1; i++) {
+            if (nodes[i] >= nodes[i+1]) return false;
+        }
+        return true;
     }
 };
