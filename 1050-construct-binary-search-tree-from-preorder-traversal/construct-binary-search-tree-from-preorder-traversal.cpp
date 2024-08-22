249275378a31b2690eb7da4827b5b4af9441fc14
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-08-22 16:56:34 +0530

    Time: 8 ms (18.29%) | Memory: 16.5 MB (52.33%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,31 +11,19 @@
  */
 class Solution {
 public:
-    int position(vector<int>& inorder, int n, int value){
-        for(int i=0; i<n; i++){
-            if(inorder[i]==value) return i;
-        }
-        return -1;
-    }
-    TreeNode* output( vector<int>& inorder, vector<int>& preorder, int &ind, int n, int start, int end){
-        if(ind>=n || start>end) return NULL;
+    TreeNode* output(vector<int>& preorder, int &ind, int bound){
+        if(ind>=preorder.size() || preorder[ind]>bound) return NULL;
 
         int element= preorder[ind]; ind++;
-        TreeNode* temp= new TreeNode(element);
-        int pos= position(inorder, n, element);
+        TreeNode* root= new TreeNode(element);
 
-        temp->left= output(inorder, preorder, ind, n, start, pos-1);
-        temp->right= output(inorder, preorder, ind, n, pos+1, end);
+        root->left= output(preorder, ind, root->val);
+        root->right= output(preorder, ind, bound);
 
-        return temp;
+        return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int n= preorder.size();
-        vector<int> inorder;
-        inorder= preorder;
-        sort(inorder.begin(), inorder.end());
         int ind=0;
-        TreeNode* ans= output(inorder, preorder, ind, n, 0, n-1);
-        return ans;
+        return output(preorder, ind, INT_MAX);
     }
 };
