f463604933152b3f97236892a2e3744fc7477bb0
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-08-26 16:15:45 +0530

    Time: 7 ms (35.11%) | Memory: 9.5 MB (64.72%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> check;
+        stack<string> check;
         string str="";
         for(int i=0; i<s.length(); i++){
             if(s[i]!=' ') str+=s[i];
             else{
                 if(str.size()!=0){
-                    check.push_back(str);
+                    check.push(str);
                     str="";
                 }
             }
         }
-        if(str.size()!=0) check.push_back(str);
-        reverse(check.begin(), check.end());
+        if(str.size()!=0) check.push(str);
         string ans="";
-        for(auto i: check) {
-            ans+=i;
+        while(!check.empty()){
+            ans+= check.top();
+            check.pop();
             ans+=" ";
         }
         ans.pop_back();
