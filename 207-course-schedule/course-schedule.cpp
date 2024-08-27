452f6dcf4635b87d1b8b6dec0cf51e8eedcb1355
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-08-28 00:02:58 +0530

    Time: 15 ms (69.51%) | Memory: 17.8 MB (37.01%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,70 @@
 class Solution {
-    private:
-    bool dfs(int node, vector<vector<int>> adj, vector<int> &vis){
-       vis[node] = 1;
-       for(auto i: adj[node]){
-            if(vis[i] == 0){
-                if(dfs(i,adj,vis)) return true;
+public:
+    // bool checkCycle(vector<vector<int>>& adj, int numCourses, int currNode, vector<int>& visited, vector<int>& pathVis){
+    //     vector<int> stack;
+    //     stack.push_back(currNode);
+
+    //     while(!stack.empty()){
+    //         int node = stack.back();
+    //         stack.pop_back();
+
+    //         if(!visited[node]){
+    //             visited[node] = 1;
+    //             pathVis[node] = 1;
+    //             stack.push_back(node);
+
+    //             for (auto v: adj[node]){
+    //                 if (!visited[v]){
+    //                     stack.push_back(v);
+    //                 }
+    //                 if(pathVis[v]){
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //         else if(pathVis[node]){
+    //             pathVis[node] = 0;
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    bool checkCycle(vector<vector<int>>& adj, int currNode, vector<int>& visited, vector<int>& pathVis){
+        visited[currNode] = 1;
+        pathVis[currNode] = 1;
+
+        for(auto v: adj[currNode]){
+            if (!visited[v]){
+                if(checkCycle(adj, v, visited, pathVis)){
+                    return true;
+                }
             }
-            else if(vis[i] == 1){
-                // cout<<"vis[node]"<<node<<endl;;
+            else if (pathVis[v]){
                 return true;
             }
-       }
-       vis[node] = 2;
-       return false;
-    }
-public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n= prerequisites.size();
-        vector<int> vis(numCourses,0);
-        vector<vector<int>> adj(numCourses);
-        for(int i=0; i<n; i++){
-            int a = prerequisites[i][0], b = prerequisites[i][1];
-            adj[b].push_back(a);
         }
-       
-        // cout<<i<<endl;
+        pathVis[currNode] = 0;
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> visited(numCourses, 0);
+        vector<int> pathVis(numCourses, 0);
+
+        for(auto v: prerequisites){
+            adj[v[0]].push_back(v[1]);
+        }
+        
         for(int i=0; i<numCourses; i++){
-            if(!vis[i]){
-                if(dfs(i, adj, vis)){
+            if (!visited[i]){
+                if(checkCycle(adj, i, visited, pathVis)==true){
                     return false;
-                }       
+                }
             }
         }
+
         return true;
     }
 };
