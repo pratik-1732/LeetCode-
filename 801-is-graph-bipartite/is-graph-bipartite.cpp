47bf364de40f73b72a1f6519d1492972c2f8fb5b
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-08-31 19:46:38 +0530

    Time: 17 ms (63.44%) | Memory: 16.3 MB (70.07%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,33 +1,33 @@
 class Solution {
     private:
-    bool bfs(vector<vector<int>>& graph, vector<int> &vis, int src){
-        vis[src]= 1;
-        queue<int> q;
-        q.push(src);
-        while(!q.empty()){
-            int node= q.front();
-            q.pop();
-            for(auto it: graph[node]){
-                if(vis[it]==-1){
-                    if(vis[node]==1) vis[it]=0;
-                    else vis[it]= 1;
-                    q.push(it);
-                    // cout<<it<<" "<<vis[it]<<" "<<node<<" "<<vis[node]<<endl;
+    bool dfs(vector<vector<int>>& graph, vector<int> &vis, int node, int color){
+        vis[node]= color;
+        // cout<<node<<" "<<color<<endl;
+        for(auto it: graph[node]){
+            if(vis[it]==-1){
+                if(vis[node]==1) {
+                    if(!dfs(graph, vis, it, 0)) return false;
                 }
-                else{
-                    if(vis[it]==vis[node]) return false;
+                else {
+                    if(!dfs(graph, vis, it, 1)) return false;
                 }
+                
+            }
+            else{
+                if(vis[it]==vis[node]) return false;
             }
         }
+
         return true;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n= graph.size();
-        vector<int> vis(n, -1);
+        vector<int> vis(n+1, -1);
+        int color= 1;
         for(int i=0; i<n; i++){
             if(vis[i]==-1) {
-                if(bfs(graph, vis, i)==false) return false;
+                if(dfs(graph, vis, i, color)==false) return false;
             }
         }
         return true;
