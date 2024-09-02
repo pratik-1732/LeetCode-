0953588ef799c4da2a8a3e4fb99809a6718d0951
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-09-02 16:06:12 +0530

    Time: 13 ms (77.69%) | Memory: 18 MB (29.87%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,39 +1,37 @@
 class Solution {
-private:
-    bool dfs(vector<vector<int>>& adj, vector<int> &vis, vector<int> &pathvis, int node, vector<int> &ans){
-        vis[node]= 1;
-        pathvis[node]= 1;
-
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                if(dfs(adj, vis, pathvis, it, ans)) return true;
-            }
-            else{
-                if(pathvis[it]) return true;
-            }
-        }
-        ans.push_back(node);
-        pathvis[node]= 0;
-        return false;
-    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses, 0);
-        vector<int> pathvis(numCourses, 0);
+        int n= prerequisites.size();
 
         vector<vector<int>> adj(numCourses+1);
-        for(int i=0; i<prerequisites.size(); i++){
+
+        for(int i=0; i<n; i++){
             vector<int> v= prerequisites[i];
             adj[v[1]].push_back(v[0]);
         }
-        vector<int> ans;
-        vector<int> empty;
+
+        vector<int> indegree(numCourses);
         for(int i=0; i<numCourses; i++){
-            if(!vis[i]){
-                if(dfs(adj, vis, pathvis, i, ans)) return empty;
+            for(auto it: adj[i]) indegree[it]++;
+        }
+        queue<int> q;
+        vector<int> ans;
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]==0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        vector<int> temp;
+
+        if(ans.size()==numCourses) return ans;
+        return temp;
     }
 };
