b2797b1f5eebcf61d3293d6418158c60138dd39e
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-10-10 22:45:35 +0530

    Time: 4 ms (15.06%) | Memory: 8.7 MB (22.07%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -7,36 +7,34 @@ public:
     }
     
     void push(int x) {
-        if(s1.empty()){
-            s1.push(x);
-            return;
-        }
-        else{
+        s1.push(x);
+    }
+    
+    int pop() {
+        if(s2.empty()){
             while(!s1.empty()){
                 s2.push(s1.top());
                 s1.pop();
             }
-            s1.push(x);
-            while(!s2.empty()){
-                s1.push(s2.top());
-                s2.pop();
-            }
         }
-    }
-    
-    int pop() {
-        if(s1.empty()) return -1;
-        int ele= s1.top();
-        s1.pop();
+        int ele= s2.top();
+        s2.pop();
         return ele;
     }
     
     int peek() {
-        return s1.top();
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        
+        return s2.top();
     }
     
     bool empty() {
-        return s1.empty();
+        return (s1.empty() && s2.empty());
     }
 };
 
