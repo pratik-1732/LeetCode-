557ca84988d738de4aa380c7a6e0295ae0e96346
Pratik Patil <105649110+pratik-1732@users.noreply.github.com>
2024-10-22 20:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 58.2 MB (67.72%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -4,32 +4,15 @@ public:
         int cnt=0, ans= INT_MIN;
         int i=0,j=0;
         int n= nums.size();
+
         while(j<n && i<n){
-            if(nums[j]==1){
-                cnt++;
-                j++;
+            if(nums[j]==0) cnt++;
+            if(cnt>k){
+                if(nums[i]==0) cnt--;
+                i++;
             }
-            else{
-                if(k>0){
-                    k--;
-                    cnt++;
-                    j++;
-                }
-                else{
-                    if(nums[i]==0 && k<=0){
-                        i++;
-                        cnt--;
-                        k++;
-                    }
-                    else{
-                        while(i<=j && nums[i]!=0){
-                            i++;
-                            cnt--;
-                        }
-                    }
-                }
-            }
-            ans= max(ans, cnt);
+            if(cnt<=k) ans= max(ans, j-i+1);
+            j++;
         }
 
         return ans;
